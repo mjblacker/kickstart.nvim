b831531b1f928174c80fb6f9d347130efa2edaac
Matt Blacker <mattjblacker@gmail.com>
2024-07-04 18:10:04 +1000

    Add in php keyword

--- init.lua
+++ init.lua
@@ -221,6 +221,13 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'php',
+  callback = function()
+    vim.opt_local.iskeyword:prepend '$'
+  end,
+})
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.hl.on_yank()`
