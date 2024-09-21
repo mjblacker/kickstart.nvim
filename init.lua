20d229d63d36d6fed4aa4c023ac5cf51b5aa8562
Matt Blacker <mattjblacker@gmail.com>
2024-09-21 14:47:22 +1000

    Init changes

--- init.lua
+++ init.lua
@@ -292,7 +292,52 @@ require('lazy').setup({
   --  This is equivalent to:
   --    require('Comment').setup({})
   
-    'BlackLight/nvim-http',
+
+  { -- Collection of various small independent plugins/modules
+    'echasnovski/mini.nvim',
+    config = function()
+      -- Better Around/Inside textobjects
+      --
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]paren
+      --  - yinq - [Y]ank [I]nside [N]ext [']quote
+      --  - ci'  - [C]hange [I]nside [']quote
+      local ai = require 'mini.ai'
+      ai.setup {
+        n_lines = 500,
+        -- custom_textobjects = {
+        --   B = MiniExtra.gen_ai_spec.buffer(),
+        --   F = ai.gen_spec.treesitter { a = '@function.outer', i = '@function.inner' },
+        -- },
+      }
+
+      -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      --
+      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+      -- - sd'   - [S]urround [D]elete [']quotes
+      -- - sr)'  - [S]urround [R]eplace [)] [']
+      require('mini.surround').setup()
+
+      -- Simple and easy statusline.
+      --  You could remove this setup call if you don't like it,
+      --  and try some other statusline plugin
+      local statusline = require 'mini.statusline'
+      -- set use_icons to true if you have a Nerd Font
+      statusline.setup { use_icons = vim.g.have_nerd_font }
+
+      -- You can configure sections in the statusline by overriding their
+      -- default behavior. For example, here we set the section for
+      -- cursor location to LINE:COLUMN
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
+
+      -- ... and there is more!
+      --  Check out: https://github.com/echasnovski/mini.nvim
+    end,
+  },
+    -- 'BlackLight/nvim-http',
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
